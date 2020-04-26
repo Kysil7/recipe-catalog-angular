e2cb1c25a07ad2a132f017d2cf047b77708b5394
Vladyslav Kysil <Vladyslav_Kysil@epam.com>
2020-04-26 21:04:27 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipieDetailComponent
   ],
   imports: [
     BrowserModule,
